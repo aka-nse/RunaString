336baaf487cdacde43a46720f8a4c9c3d5b5d0d0
Daishi Nakase <nakase.d1220@gmail.com>
2026-04-05 21:27:12 +0900

    adds test

---
+++ tests/RunaString.Test/RuneIndexTest.cs
@@ -0,0 +1,90 @@
+using System.Runtime.CompilerServices;
+
+namespace RunaString.Test;
+
+public partial class RuneIndexTest
+{
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = ".ctor")]
+    private static extern void ctor_Utf8Index(ref Utf8RuneIndex index, int byteIndex, int runePosition);
+    private static Utf8RuneIndex CreateUtf8Index(int byteIndex, int runePosition)
+    {
+        Utf8RuneIndex index = default;
+        ctor_Utf8Index(ref index, byteIndex, runePosition);
+        return index;
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = ".ctor")]
+    private static extern void ctor_Utf16Index(ref Utf16RuneIndex index, int charIndex, int runePosition);
+    private static Utf16RuneIndex CreateUtf16Index(int charIndex, int runePosition)
+    {
+        Utf16RuneIndex index = default;
+        ctor_Utf16Index(ref index, charIndex, runePosition);
+        return index;
+    }
+
+    [UnsafeAccessor(UnsafeAccessorKind.Method, Name = ".ctor")]
+    private static extern void ctor_Utf32Index(ref Utf32RuneIndex index, int runePosition);
+    private static Utf32RuneIndex CreateUtf32Index(int runePosition)
+    {
+        Utf32RuneIndex index = default;
+        ctor_Utf32Index(ref index, runePosition);
+        return index;
+    }
+
+    public static TheoryData<ISeekIndex, ISeekIndex, int> EquatableComparableTestCase() =>
+        new()
+        {
+            { CreateUtf8Index(0, 0), CreateUtf8Index(0, 0), 0 },
+            { CreateUtf8Index(1, 1), CreateUtf8Index(1, 1), 0 },
+            { CreateUtf8Index(100, 50), CreateUtf8Index(100, 50), 0 },
+            { CreateUtf8Index(10, 5), CreateUtf8Index(100, 50), -1 },
+            { CreateUtf8Index(100, 50), CreateUtf8Index(10, 5), 1 },
+
+            { CreateUtf16Index(0, 0), CreateUtf16Index(0, 0), 0 },
+            { CreateUtf16Index(1, 1), CreateUtf16Index(1, 1), 0 },
+            { CreateUtf16Index(60, 50), CreateUtf16Index(60, 50), 0 },
+            { CreateUtf16Index(6, 5), CreateUtf16Index(60, 50), -1 },
+            { CreateUtf16Index(60, 50), CreateUtf16Index(6, 5), 1 },
+
+            { CreateUtf32Index(0), CreateUtf32Index(0), 0 },
+            { CreateUtf32Index(1), CreateUtf32Index(1), 0 },
+            { CreateUtf32Index(50), CreateUtf32Index(50), 0 },
+            { CreateUtf32Index(5), CreateUtf32Index(50), -1 },
+            { CreateUtf32Index(50), CreateUtf32Index(5), 1 },
+        };
+
+
+    [Theory]
+    [MemberData(nameof(EquatableComparableTestCase))]
+    public void Equal<TIndex>(TIndex x, TIndex y, int expected)
+        where TIndex : unmanaged, ISeekIndex<TIndex>
+    {
+        var exp = expected == 0;
+        Assert.Equal(exp, TIndex.Equals(x, y));
+        Assert.Equal(exp, x.Equals(y));
+        Assert.Equal(exp, y.Equals(x));
+        Assert.Equal(exp, x.Equals((object)y));
+        Assert.Equal(exp, y.Equals((object)x));
+        Assert.Equal(exp, x == y);
+        Assert.Equal(!exp, x != y);
+        if (exp)
+        {
+            Assert.Equal(x.GetHashCode(), y.GetHashCode());
+        }
+    }
+
+
+    [Theory]
+    [MemberData(nameof(EquatableComparableTestCase))]
+    public void Compare<TIndex>(TIndex x, TIndex y, int expected)
+        where TIndex : unmanaged, ISeekIndex<TIndex>
+    {
+        Assert.Equal(Math.Sign(expected), Math.Sign(TIndex.Compare(x, y)));
+        Assert.Equal(Math.Sign(expected), Math.Sign(x.CompareTo(y)));
+        Assert.Equal(-Math.Sign(expected), Math.Sign(y.CompareTo(x)));
+        Assert.Equal(expected < 0, x < y);
+        Assert.Equal(expected > 0, x > y);
+        Assert.Equal(expected <= 0, x <= y);
+        Assert.Equal(expected >= 0, x >= y);
+    }
+}
