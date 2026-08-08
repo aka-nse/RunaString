a992f8d0f9752e96686eafc48f9354eb0bf0dde6
Daishi Nakase <nakase.d1220@gmail.com>
2026-08-08 15:18:22 +0900

    removes unused API

--- src/RunaString/RunaString._Utf8.cs
+++ src/RunaString/RunaString._Utf8.cs
@@ -96,12 +96,6 @@ public readonly partial struct Utf8String
         return new(Buffer.Slice(byteStart, byteLength));
     }
 
-    internal readonly Utf8String DangerousSlice(int byteStart, int byteLength)
-    {
-        return new(Buffer.Slice(byteStart, byteLength));
-    }
-
-
     /// <inheritdoc />
     public bool TryGetRune(Utf8Index index, out Rune rune, out int codeUnitConsumed) =>
         FileHelpers.TryGetRune(Buffer.Span, index, out rune, out codeUnitConsumed);
